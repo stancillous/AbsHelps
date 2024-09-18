ecc1cd2d0c812f78ae100d4fb24a0f606f1c8279
stancillous <stancillousray@gmail.com>
2024-09-18 23:32:50 +0300

    view that will toggle btwn register and login components

---
+++ components/loginRegisterView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+          <transition name="fade">
+                  <component :is="activeComponent" />
+            </transition>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const loginView = resolveComponent('Login')
+const registerView = resolveComponent('Register')
+
+const componentsMap = {
+    loginView: loginView,
+    registerView: registerView
+}
+
+const appStore = useStore()
+
+const activeComponent = computed(()=>{
+    return componentsMap[appStore.currentTab] || loginView
+})
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
