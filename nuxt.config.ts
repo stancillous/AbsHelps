958b8fc8a595b7d1aad640c1e4238e019a6dbb13
stancillous <stancillousray@gmail.com>
2024-09-18 23:32:15 +0300

    use pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  modules: ["@bootstrap-vue-next/nuxt"],
+  modules: [
+    "@bootstrap-vue-next/nuxt",
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "acceptHMRUpdate"],
+      },
+    ],
+  ],
   css: ["~/assets/styles/main.css", "bootstrap/dist/css/bootstrap.min.css"],
 });
