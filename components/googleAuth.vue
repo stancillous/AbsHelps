b14f875dfbb5fb2b49b024c7cc10b82ead011d67
stancillous <stancillousray@gmail.com>
2024-09-20 00:35:20 +0300

    social auth buttons

---
+++ components/googleAuth.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+
+         <div class="social-wrp">
+              <div class="wrap_text-icon">
+                <img
+                width="24px"
+                  src="https://cdn.prod.website-files.com/6490500a0362c1a424db6d08/649050e81600d893ba045ccd_googlesvg.svg"
+                  loading="lazy"
+                  alt=""
+                  class="google-icon"
+                />
+                <div>Continue with Google</div>
+              </div>
+            </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+
+.social-wrp{
+  
+  .wrap_text-icon{
+    display: flex;
+    align-items: center;
+    border: 1px solid gainsboro;
+    justify-content: center;
+    font-size: 1.5rem;
+    border-radius: 10rem;
+    img{
+      margin-right: 3rem;
+      height: 51px;
+    }
+
+
+  }
+}
+</style>
